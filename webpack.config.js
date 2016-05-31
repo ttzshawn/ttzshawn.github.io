8ed642cad8e0aaf20db3d0fa90d5017f015a724c
Shawn.Zhang <shawnzhang009@gmail.com>
2016-06-01 00:05:22 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
-var webpack = require('webpack');
 var path = require('path');
+var webpack = require('webpack');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
-
-var config = {
-    entry: APP_DIR + '/index.jsx',
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    }
+module.exports = {
+    entry: './main.js',
+    output: { path: __dirname, filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
 };
-
-module.exports = config;
